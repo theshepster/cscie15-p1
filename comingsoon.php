bd7067c89a2aa8008c817bfb6c7852be109b94f9
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-09-10 20:53:24 -0400

    Following DRY principals, moved header to own file The header is shared between the index.html and comingsoon.html files. This necessitated changing index.html to index.php and comingsoon.html to comingsoon.php to utilize the include_once functions. (Not trying to get too far ahead of myself, just trying to follow the requested programming principles.)

---
+++ comingsoon.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<?php include_once("header.html"); ?>
+    <body>
+         <div class="row text-center">
+            <div class="small-12 columns">
+                <h1>Coming soon!</h1>
+                <h3><a href="./">Back to portfolio</a></h3>
+            </div>
+        </div>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/what-input.js"></script>
+        <script src="js/vendor/foundation.js"></script>
+        <script src="js/app.js"></script>
+    </body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<?php include_once("header.html"); ?>
+    <body>
+
+        <section class="hero">  
+        </section>
+
+        <div class="row text-center">
+            <div class="small-12 columns">
+                <h1>Shep Moore-Berg</h1>
+                <h2 class="subheader">Portfolio</h2>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="medium-4 columns">
+                <div class="profile-card">
+                <img src="img/p2-logo.png" alt="P2">
+                    <div class="profile-info">
+                        <h4 class="subheader">Project 2</h4>
+                        <p>Placeholder description for Project 2.</p>
+                        <p><a href="comingsoon.php">Site</a> | <a href="comingsoon.php">Github</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="profile-card">
+                <img src="img/p3-logo.png" alt="P2">
+                    <div class="profile-info">
+                        <h4 class="subheader">Project 3</h4>
+                        <p>Placeholder description for Project 3.</p>
+                        <p><a href="comingsoon.php">Site</a> | <a href="comingsoon.php">Github</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="profile-card">
+                <img src="img/p4-logo.png" alt="P2">
+                    <div class="profile-info">
+                        <h4 class="subheader">Project 4</h4>
+                        <p>Placeholder description for Project 4.</p>
+                        <p><a href="comingsoon.php">Site</a> | <a href="comingsoon.php">Github</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/what-input.js"></script>
+        <script src="js/vendor/foundation.js"></script>
+        <script src="js/app.js"></script>
+    </body>
+</html>
+
+
